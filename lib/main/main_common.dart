a2af9a9abc0197c08e0ed2671389ed7d7769fb04
AdamFakee <buidinhtuan04@gmail.com>
2025-08-26 15:56:15 +0700

    register firebase & install necesslly lib for auth google/facebook

--- lib/main/main_common.dart
+++ lib/main/main_common.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:strava/firebase_options.dart';
 import 'package:strava/main/app.dart';
 import 'package:strava/main/app_env.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 /// func used for every environment ( dev or product )
 Future<void> mainCommon (SAppEnvEnum environment) async {
   // init env
   await SAppEnv.init(environment);
+
+  // init firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then(
+    (_) => {}
+  );
 
   runApp(ProviderScope(child: MyApp()));
 }
